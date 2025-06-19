d84a5baa2dc23eeb00314d1ec7eaa0ea78eb9e5c
meera0305 <chavanmeera66@gmail.com>
2025-06-19 15:32:38 +0530

    Create joins_queries.sql

---
+++ joins_queries.sql
@@ -0,0 +1,10 @@
+CREATE TABLE courses (
+  course_id INT AUTO_INCREMENT PRIMARY KEY,
+  course_name VARCHAR(100)
+);
+
+INSERT INTO courses (course_name) VALUES ('Java'), ('Python'), ('MySQL');
+
+SELECT s.name, c.course_name 
+FROM students s 
+JOIN courses c ON s.course = c.course_name;
