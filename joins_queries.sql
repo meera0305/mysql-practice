fcddc1e574aa27785ba9775358cdce9247c6e13d
Meera Santram Chavan <chavanmeera66@gmail.com>
2025-08-23 08:53:33 +0530

    Update joins_queries.sql

--- joins_queries.sql
+++ joins_queries.sql
@@ -1,8 +1,7 @@
 CREATE TABLE courses (
   course_id INT AUTO_INCREMENT PRIMARY KEY,
   course_name VARCHAR(100)
-);
-
+);   
 INSERT INTO courses (course_name) VALUES ('Java'), ('Python'), ('MySQL');
 SELECT s.name, c.course_name 
 FROM students s 
