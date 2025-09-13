fd25e19c3ebe847382ab6c8fbc887163cf8452ed
Meera Santram Chavan <chavanmeera66@gmail.com>
2025-09-13 21:16:16 +0530

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS my_practice_db;
 USE my_practice_db;
    
 CREATE TABLE students (   
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,  
   name VARCHAR(100),
   age INT,
   course VARCHAR(100)      
